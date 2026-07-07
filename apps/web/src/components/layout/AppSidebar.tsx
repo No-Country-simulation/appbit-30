3627d596d0756e4b02399941cb759dc9f1393e58
CristhianSZ <cristhiansosazurita@gmail.com>
2026-07-07 18:16:56 -0300

    feat(mentoria): update sidebar nav link to /mentoria

--- apps/web/src/components/layout/AppSidebar.tsx
+++ apps/web/src/components/layout/AppSidebar.tsx
@@ -24,7 +24,7 @@ const navGroups = [
   ],
   [
     { labelKey: 'navExperiencias', icon: Star, href: '#' },
-    { labelKey: 'navMentorias', icon: Users, href: '#' },
+    { labelKey: 'navMentorias', icon: Users, href: '/mentoria' },
   ],
   [{ labelKey: 'navBienestar', icon: Heart, href: '#' }],
 ];
